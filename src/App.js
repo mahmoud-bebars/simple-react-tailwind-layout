071a743cc498169a4ed46f2b84d70120f7d93ec5
m.bebars <66588352+mahmoud-bebars@users.noreply.github.com>
2023-03-10 21:11:29 +0200

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {/* Main Frame */}
+      <div className='main-frame'>
+        {/* Header */}
+        <header className='header'>Header</header>
+        {/* Navogation */}
+        <nav className='nav'>navigation</nav>
+        {/* Main content + Sidebar */}
+        <div className='flex'>
+          {/* Main content */}
+          <div className='main-content'>
+            main content
+            <div className='article p-14'> article</div>
+            <div className='article p-12'> article</div>
+            <div className='article p-10'> article</div>
+            <div className='article p-8'> article</div>
+          </div>
+          {/* Sidebar */}
+          <div className='sidebar'>sidebar</div>
+        </div>
+        {/* Footer */}
+        <footer className='footer'>footer</footer>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
